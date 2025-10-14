e6fa2016931d3fdd63cfa86b617aeff6ffd9227d
Simon Tönnes <simon.tonnes@forefront.se>
2025-10-14 14:24:20 +0200

    railway oriented code

---
+++ src/eWorldCup.Core/RailwayOriented/Example.cs
@@ -0,0 +1,41 @@
+﻿namespace eWorldCup.Core.RailwayOriented;
+
+public class Example
+{
+    public Result<ReturnObjekt> DoAllTheStuff()
+    {
+        return GetSomething()
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething)
+            .OnSuccess(ChangeSomething);
+    }
+    
+    public Result<ReturnObjekt> GetSomething()
+    {
+        var o = new ReturnObjekt("hej");
+        
+        return Result<ReturnObjekt>.Success(o);
+    }
+    
+    public Result<ReturnObjekt> ChangeSomething(ReturnObjekt anObject)
+    {
+        anObject.Count++;
+
+        return Result<ReturnObjekt>.Success(anObject);
+    }
+    
+}
+
+public record ReturnObjekt(string Name)
+{
+    public int Count { get; set; }
+}
