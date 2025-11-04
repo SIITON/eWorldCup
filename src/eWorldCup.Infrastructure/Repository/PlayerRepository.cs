f0fecf0d1fceea7e0a7c26d1860a26b202f4300d
Simon Tönnes <simon.tonnes@forefront.se>
2025-11-04 12:01:07 +0100

    Moved seeding to DI

--- src/eWorldCup.Infrastructure/Repository/PlayerRepository.cs
+++ src/eWorldCup.Infrastructure/Repository/PlayerRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Text.Json;
 using eWorldCup.Core.Interfaces.Repositories;
 using eWorldCup.Core.Models;
 
@@ -7,43 +6,15 @@ namespace eWorldCup.Infrastructure.Repository;
 
 public class PlayerRepository : IPlayerRepository
 {
-    internal record PlayerSeed(int Id, string Name);
-
-    private const string Seed = """
-                                 [
-                                   { "id": 1, "name": "Alice" },
-                                   { "id": 2, "name": "Bob" },
-                                   { "id": 3, "name": "Charlie" },
-                                   { "id": 4, "name": "Diana" },
-                                   { "id": 5, "name": "Ethan" },
-                                   { "id": 6, "name": "Fiona" },
-                                   { "id": 7, "name": "George" },
-                                   { "id": 8, "name": "Hannah" },
-                                   { "id": 9, "name": "Isaac" },
-                                   { "id": 10, "name": "Julia" },
-                                   { "id": 11, "name": "Kevin" },
-                                   { "id": 12, "name": "Laura" },
-                                   { "id": 13, "name": "Michael" },
-                                   { "id": 14, "name": "Nina" },
-                                   { "id": 15, "name": "Oscar" },
-                                   { "id": 16, "name": "Paula" },
-                                   { "id": 17, "name": "Quentin" },
-                                   { "id": 18, "name": "Rachel" },
-                                   { "id": 19, "name": "Samuel" },
-                                   { "id": 20, "name": "Tina" }
-                                 ]
-                                 """;
-
     internal ConcurrentDictionary<int, string> Players = new();
     internal int NextId() => Players.Keys.DefaultIfEmpty(0).Max() + 1;
-
-    public PlayerRepository()
+    
+    public void Seed(IEnumerable<(int Id, string Name)> players)
     {
-        // seed initial players
-        var players = JsonSerializer.Deserialize<List<PlayerSeed>>(Seed) ?? throw new NullReferenceException();
-        foreach (var player in players)
+        if (!Players.IsEmpty) return;
+        foreach (var (id, name) in players)
         {
-            Players[player.Id] = player.Name;
+            Players[id] = name;
         }
     }
 
