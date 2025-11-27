83165d53b7e19d0218a9d929f902445149634b71
Simon Tönnes <simon.tonnes@forefront.se>
2025-11-27 18:32:17 +0100

    Implemented tournament repository

--- src/eWorldCup.Infrastructure/Repository/TournamentRepository.cs
+++ src/eWorldCup.Infrastructure/Repository/TournamentRepository.cs
@@ -1,32 +1,82 @@
-﻿using eWorldCup.Core.Interfaces.Repositories;
+﻿using System.Collections.Concurrent;
+using eWorldCup.Core.Interfaces.Repositories;
 using eWorldCup.Core.Models.Games.RockPaperArena;
+using eWorldCup.Infrastructure.ResponseModels;
 
 namespace eWorldCup.Infrastructure.Repository;
 
 public class TournamentRepository : ITournamentRepository
 {
+    internal ConcurrentDictionary<Guid, TournamentResponseModel> Tournaments = new();
     public RockPaperArenaTournament Add(RockPaperArenaTournament value)
     {
-        throw new NotImplementedException();
+        var id = value.TournamentId;
+        if (!Tournaments.TryAdd(id, Map(value)))
+        {
+            throw new ArgumentException($"Tournament with ID {id} already exists.");
+        }
+
+        return Get(id);
     }
 
     public RockPaperArenaTournament Get(Guid id)
     {
-        throw new NotImplementedException();
+        if (Tournaments.TryGetValue(id, out var tournament))
+        {
+            return Map(tournament);
+        }
+        throw new KeyNotFoundException($"Tournament with ID {id} not found.");
     }
 
     public IEnumerable<RockPaperArenaTournament> GetAll()
     {
-        throw new NotImplementedException();
+        return Tournaments.Select(kv => Map(kv.Value));
     }
 
     public RockPaperArenaTournament Update(RockPaperArenaTournament value)
     {
-        throw new NotImplementedException();
+        var id = value.TournamentId;
+        Tournaments[id] = Map(value);
+        
+        return Get(id);
     }
 
     public bool Delete(Guid id)
     {
-        throw new NotImplementedException();
+        if (!Tournaments.TryRemove(id, out _))
+        {
+            throw new KeyNotFoundException($"Tournament with ID {id} not found.");
+        }
+
+        return true;
+    }
+
+    internal TournamentResponseModel Map(RockPaperArenaTournament value)
+    {
+        var currentMatch = value.Schedule.GetMatchesForPlayer(1).ToList()[(int)value.CurrentRound];
+        return new TournamentResponseModel
+        {
+            Id = value.TournamentId.ToString(),
+            NumberOfPlayers = value.NumberOfPlayers,
+            CurrentRound = 0,
+            CurrentMatch = new CurrentMatchResponseModel
+            {
+                Round = 0,
+                PlayerOneScore = 0,
+                PlayerTwoScore = 0
+            },
+            IsFinished = false,
+            PlayerScores = null
+        };
+    }
+
+    internal RockPaperArenaTournament Map(TournamentResponseModel value)
+    {
+        var id = Guid.Parse(value.Id);
+        return new RockPaperArenaTournament(value.NumberOfPlayers, id)
+        {
+            CurrentRound = value.CurrentRound
+        };
     }
 }
+
