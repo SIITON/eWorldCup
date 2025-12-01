b19ec0e98e75ba01aa3e78cadea317d865f23caf
Simon Tönnes <simon.tonnes@forefront.se>
2025-12-01 18:03:28 +0100

    Added logic for /status endpoint

--- src/eWorldCup.Application/Features/RockPaperArena/GetTournamentStatusRequest.cs
+++ src/eWorldCup.Application/Features/RockPaperArena/GetTournamentStatusRequest.cs
@@ -1,8 +1,34 @@
-﻿using MediatR;
+﻿using eWorldCup.Core.Interfaces.Repositories;
+using eWorldCup.Core.Models.API.Responses;
+using MediatR;
 
 namespace eWorldCup.Application.Features.RockPaperArena;
 
-public class GetTournamentStatusRequest(Guid tournamentId) : IRequest<bool>
+public class GetTournamentStatusRequest(Guid tournamentId) : IRequest<TournamentStatusResponse>
 {
     public Guid TournamentId { get; set; } = tournamentId;
+}
+
+public class GetTournamentStatusHandler(ITournamentRepository tournaments) : IRequestHandler<GetTournamentStatusRequest, TournamentStatusResponse>
+{
+    public async Task<TournamentStatusResponse> Handle(GetTournamentStatusRequest request, CancellationToken cancellationToken)
+    {
+        var tournament = tournaments.Get(request.TournamentId);
+
+        var scoresByIndex = tournament.Scores.ScoresByPlayerIndex;
+        var participants = tournament.Participants.Select(((player, idx) => new PlayerScoreResponse
+        {
+            Id = player.Id,
+            Index = idx,
+            Name = player.Name,
+            Score = scoresByIndex.GetValueOrDefault(idx, -1)
+        }));
+
+        var status = new TournamentStatusResponse()
+        {
+            CurrentRound = tournament.CurrentRound,
+            Participants = participants.ToList()
+        };
+        return await Task.FromResult(status);
+    }
 }
---
+++ src/eWorldCup.Core/Models/API/Responses/TournamentStatusResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace eWorldCup.Core.Models.API.Responses;
+
+public class TournamentStatusResponse
+{
+    public IList<PlayerScoreResponse> Participants { get; set; } = [];
+    public long CurrentRound { get; set; }
+}
+
+public class PlayerScoreResponse
+{
+    public int Id { get; set; }
+    public int Index { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Score { get; set; }
+}
