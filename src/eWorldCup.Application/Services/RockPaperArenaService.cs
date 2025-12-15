a237f34e5afa7f47906ed57215bec020ba06e976
Simon Tönnes <simon.tonnes@forefront.se>
2025-12-15 16:02:19 +0100

    refactored PlayNextRoundHandler, moved logic to RockPaperArenaService

--- src/eWorldCup.Application/Services/RockPaperArenaService.cs
+++ src/eWorldCup.Application/Services/RockPaperArenaService.cs
@@ -1,6 +1,7 @@
 ﻿using eWorldCup.Core.Interfaces.Repositories;
 using eWorldCup.Core.Models;
 using eWorldCup.Core.Models.Games.RockPaperArena;
+using eWorldCup.Core.Models.Games.RockPaperArena.Exceptions;
 
 namespace eWorldCup.Application.Services;
 
@@ -24,6 +25,48 @@ public class RockPaperArenaService(IPlayerRepository playerRepository,
         return tournament;
     }
 
+    public RoundResults PlayRound(Guid tournamentId, HandShape playerMove)
+    {
+        var tournament = tournamentRepository.Get(tournamentId);
+        var match = tournament.GetUserMatch();
+        if (match.IsOver(tournament.Settings.MaximumRoundsInAMatch))
+        {
+            throw new MatchOverException();
+        }
+        var playerHand = new Hand().Show(playerMove);
+        var opponentHand = new Hand().Randomize();
+        var results = playerHand.Versus(opponentHand);
+        match.UpdateScore(results);
+        var matchIsOver = match.IsOver(tournament.Settings.MaximumRoundsInAMatch);
+        // store the results
+        tournament.CurrentMatch = match;
+        Player? winningPlayer = null;
+        if (matchIsOver)
+        {
+            tournament.RegisterFinishedMatch(match);
+            winningPlayer = tournament.GetParticipantByIndex(match.GetWinnerIndex());
+        }
+        tournamentRepository.Update(tournament);
+        
+        return new RoundResults
+        {
+            CurrentRound = (int)match.RoundNumber,
+            NumberOfRoundsPlayed = match.NumberOfRoundsPlayed,
+            IsMatchOver = matchIsOver,
+            PlayerMove = playerHand.Shape,
+            OpponentMove = opponentHand.Shape,
+            IsDraw = results.IsDraw,
+            IsPlayerWin = results.PlayerOneWins,
+            IsOpponentWin = results.PlayerTwoWins,
+            Score = new RoundScore()
+            {
+                Player = match.Score.Player,
+                Opponent = match.Score.Opponent
+            },
+            Winner = winningPlayer,
+        };
+    }
+
     internal List<Player> GetTournamentOpponents(int numberOfPlayers, Player user)
     {
         var opponents = playerRepository.GetAll()
@@ -38,4 +81,5 @@ public class RockPaperArenaService(IPlayerRepository playerRepository,
 public interface IRockPaperArenaService
 {
     RockPaperArenaTournament Start(string playerName, int numberOfPlayers);
+    RoundResults PlayRound(Guid tournamentId, HandShape playerMove);
 }
---
+++ src/eWorldCup.Core/Models/Games/RockPaperArena/RoundResults.cs
@@ -0,0 +1,22 @@
+﻿namespace eWorldCup.Core.Models.Games.RockPaperArena;
+
+public class RoundResults
+{
+    public int CurrentRound { get; set; }
+    public int NumberOfRoundsPlayed { get; set; }
+    public bool IsMatchOver { get; set; }
+    public HandShape PlayerMove { get; init; } 
+    public HandShape OpponentMove { get; init; }
+    public bool IsDraw { get; init; }
+    public bool IsPlayerWin { get; init; }
+    public bool IsOpponentWin { get; set; }
+    public RoundScore Score { get; set; } = new();
+    public Player? Winner { get; set; }
+}
+
+public class RoundScore
+{
+
+    public int Player { get; set; }
+    public int Opponent { get; set; }
+}
